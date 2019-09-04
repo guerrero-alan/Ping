6be42284179b4629ace35bfaebaf04dd25190966
Alan <guerrero_alan@outlook.com>
2019-09-04 18:12:52 -0500

    Initial Commit

---
+++ Ping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ping
+//
+//  Created by Alan Guerrero on 9/4/19.
+//  Copyright © 2019 Alan Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
